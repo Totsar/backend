7a02a6bd23d78734d09945d23653336b1ca0813e
Alireza Samimi <alirezasamimi100@yahoo.com>
2026-01-05 00:50:27 +0330

    Tidy up 2

--- core/models.py
+++ core/models.py
@@ -1,8 +1,11 @@
 from django.db import models
-from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    pass
 
 class Item(models.Model):
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="items")
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="items")
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
--- core/serializers.py
+++ core/serializers.py
@@ -1,6 +1,5 @@
-from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import Item
+from .models import Item, User
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
