def2ade51ca854b83df97f4b330fbd6f1b8f4d12
Alireza Samimi <alirezasamimi100@yahoo.com>
2026-02-18 18:48:04 +0330

    Add CORS header

--- backend/settings.py
+++ backend/settings.py
@@ -42,11 +42,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -147,5 +149,7 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
+
+CORS_ALLOWED_ORIGINS = os.getenv('DJANGO_ALLOWED_ORIGINS', 'http://localhost:5173/')
 
 AUTH_USER_MODEL = 'core.User'
